0efe5a8eccdb7758f4bcca3a4b86b91e4909b035
danagh <dana.ghabeti@gmail.com>
2017-04-03 17:42:30 +0200

    added changing headersize

--- javascript/index.js
+++ javascript/index.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+    init();
     createEventHandlers();
 });
 
@@ -7,6 +8,27 @@ function createEventHandlers() {
         document.querySelector('#about-id').scrollIntoView({
             behavior: 'smooth'
         });
+    });
+}
+
+function init() {
+    window.addEventListener('scroll', function(e){
+        var distanceY = window.pageYOffset || document.documentElement.scrollTop,
+            shrinkOn = 300,
+            header = document.querySelector(".header");
+        if (distanceY > shrinkOn) {
+            $('.header').addClass('smaller');
+            $('.header-text').addClass('smaller');
+            $('.about').addClass('smaller');
+            $('.login').addClass('smaller');
+        } else {
+            if ($('.header').hasClass('smaller') && $('.header-text').hasClass('smaller') && $('.about').hasClass('smaller') && $('.login').hasClass('smaller')  ) {
+                $('.header').removeClass('smaller');
+                $('.header-text').removeClass('smaller');
+                $('.about').removeClass('smaller');
+                $('.login').removeClass('smaller');
+            }
+        }
     });
 }
 
