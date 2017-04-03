93f8b602ee4423135b5dcbf4a61bdf948b050ed1
danagh <dana.ghabeti@gmail.com>
2017-04-03 16:46:41 +0200

    added scroll function

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,14 @@
+$(document).ready(function() {
+    createEventHandlers();
+});
+
+function createEventHandlers() {
+    $(document).on('click', '.about', function(){
+        document.querySelector('#about-id').scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+}
+
+
 
