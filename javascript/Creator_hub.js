26a3a1feac422bc08cd7fe23bea06589844f1823
danagh <dana.ghabeti@gmail.com>
2017-04-05 15:39:25 +0200

    added drag and drop

--- javascript/Creator_hub.js
+++ javascript/Creator_hub.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+    addAttributesToWeatherOptionDiv();
     createEventHandlers();
 });
 
@@ -22,9 +23,52 @@ function createEventHandlers() {
 
 }
 
-$(function(){
-    // Find selected div
-    var selected = $('#scroll .selected');
-    // Scroll container to offset of the selected div
-    selected.parent().parent().scrollTop(selected[0].offsetTop);
-});
+// $(function(){
+//     // Find selected div
+//     var selected = $('#scroll .selected');
+//     // Scroll container to offset of the selected div
+//     selected.parent().parent().scrollTop(selected[0].offsetTop);
+// });
+
+
+function addAttributesToWeatherOptionDiv() {
+    var numberOfDivs = document.querySelectorAll('.weather-option').length;
+    for (var i=0;i<numberOfDivs;i++) {
+        console.log('loop number: ' + i);
+        var selectedDiv = document.getElementsByClassName('weather-option')[i];
+        var iInt = i.toString();
+        selectedDiv.setAttribute('weatherId', iInt);
+    }
+
+
+}
+
+function drag(ev) {
+    var weatherId = ev.target.getAttribute("weatherId"); //get the drinkId
+    ev.dataTransfer.setData("weather-id", weatherId);
+
+
+}
+
+/*
+ Allows a drink-div to be dropped into the order-summary side of the page. If this
+ function is not called it won't be droppable. Go
+ */
+function allowDrop(ev) {
+    ev.preventDefault();
+
+}
+
+/*
+ When a drink is dropped it has to give the drinkId. We then call the findDrinkById-function
+ to get the drinkPrice and drinkName so thatwe can show the correct information in
+ the order summary. To show the added drinks we call the order.addItem-function that we created.
+ */
+function drop(ev) {
+    ev.preventDefault();
+    var draggedWeatherId = ev.dataTransfer.getData("weather-id");
+    console.log(draggedWeatherId);
+    
+
+
+}
