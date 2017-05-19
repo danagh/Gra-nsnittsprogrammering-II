1dcfdf24fcf5bfee929195b01a64d9b7a6649e0b
Magto <martin@magto.se>
2017-05-19 16:11:16 +0200

    Mindre opass

--- javascript/Lifeganizer_premade.js
+++ javascript/Lifeganizer_premade.js
@@ -2,13 +2,13 @@
  * Created by martin on 2017-05-19.
  */
 var fontFamilies=["Alex Brush","Diplomata","Diplomata"];
-var objectLeftPositions=["412.0057002939405px","469px","23.9747px","22.984px","19.9844px","115.984px","118px","117px","117px","414px","398px","7px","19px"];
+var objectLeftPositions=["412.0057002939405px","469px","23px","23px","23px","115.984px","118px","117px","117px","414px","398px","7px","23px"];
 var objectFont=["Alex Brush","Diplomata","noFont","noFont","noFont","noFont","noFont","noFont","noFont","Diplomata","noFont","noFont","noFont"];
-var objectHeight=["87px","61px","40px","40px","40px","64.4375px","62.8125px","61.9844px","62.7969px","74.6094px","77px","272.391px","42.5px"];
+var objectHeight=["87px","61px","40px","40px","40px","64.4375px","62.8125px","61.9844px","62.7969px","74.6094px","77px","272.391px","40px"];
 var objectId=["1","2","3","4","5","7","8","9","10","11","12","13","14"];
 var textMessages=[null,null,null,null,null,null,null,null,null,null,null,null,null];
 var objectStyles=["clock","date","weather","weather","weather","temperature","temperature","temperature","temperature","temperature","weather","temp-graph","weather"];
-var objectWidth=["174px","122px","65px","69px","76px","65.9062px","77.0938px","76.0938px","77.0938px","137.969px","154px","454px","85px"];
+var objectWidth=["174px","122px","69px","69px","69px","65.9062px","77.0938px","76.0938px","77.0938px","137.969px","154px","454px","69px"];
 var seconds=["true",null,null,null,null,null,null,null,null,null,null,null,null];
 var objectTopPositions=["210.6150752939405px","433.1875px","306.662px","224.234px","55.266px","63.2188px","152.297px","237.188px","321.188px","180.188px","63.1875px","432.515625px","141.188px"];
 var weatherTime=[null,null,"evening","afternoon","morning","morning","midday","afternoon","evening","current-time","current-time","whole-day","midday"];
